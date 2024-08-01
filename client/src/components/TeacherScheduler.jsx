138da176541b65413f79d550a511287aa0c4d98f
WRISH <wrishavnandi.work@gmail.com>
2024-08-01 14:18:35 +0530

    commit test

--- client/src/components/TeacherScheduler.jsx
+++ client/src/components/TeacherScheduler.jsx
@@ -23,133 +23,100 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 const appointments = [
-    {
-      title: 'Website Re-Design Plan',
-      startDate: new Date(2018, 6, 23, 9, 30),
-      endDate: new Date(2018, 6, 23, 11, 30),
-    }, {
-      title: 'Book Flights to San Fran for Sales Trip',
-      startDate: new Date(2018, 6, 23, 12, 0),
-      endDate: new Date(2018, 6, 23, 13, 0),
-    }, {
-      title: 'Install New Router in Dev Room',
-      startDate: new Date(2018, 6, 23, 14, 30),
-      endDate: new Date(2018, 6, 23, 15, 30),
-    }, {
-      title: 'Approve Personal Computer Upgrade Plan',
-      startDate: new Date(2018, 6, 24, 10, 0),
-      endDate: new Date(2018, 6, 24, 11, 0),
-    }, {
-      title: 'Final Budget Review',
-      startDate: new Date(2018, 6, 24, 12, 0),
-      endDate: new Date(2018, 6, 24, 13, 35),
-    }, {
-      title: 'New Brochures',
-      startDate: new Date(2018, 6, 24, 14, 30),
-      endDate: new Date(2018, 6, 24, 15, 45),
-    }, {
-      title: 'Install New Database',
-      startDate: new Date(2018, 6, 25, 9, 45),
-      endDate: new Date(2018, 6, 25, 11, 15),
-    }, {
-      title: 'Approve New Online Marketing Strategy',
-      startDate: new Date(2018, 6, 25, 12, 0),
-      endDate: new Date(2018, 6, 25, 14, 0),
-    }, {
-      title: 'Upgrade Personal Computers',
-      startDate: new Date(2018, 6, 25, 15, 15),
-      endDate: new Date(2018, 6, 25, 16, 30),
-    }, {
-      title: 'Customer Workshop',
-      startDate: new Date(2018, 6, 26, 11, 0),
-      endDate: new Date(2018, 6, 26, 12, 0),
-    }, {
-      title: 'Prepare 2015 Marketing Plan',
-      startDate: new Date(2018, 6, 26, 11, 0),
-      endDate: new Date(2018, 6, 26, 13, 30),
-    }, {
-      title: 'Brochure Design Review',
-      startDate: new Date(2018, 6, 26, 14, 0),
-      endDate: new Date(2018, 6, 26, 15, 30),
-    }, {
-      title: 'Create Icons for Website',
-      startDate: new Date(2018, 6, 27, 10, 0),
-      endDate: new Date(2018, 6, 27, 11, 30),
-    }, {
-      title: 'Upgrade Server Hardware',
-      startDate: new Date(2018, 6, 27, 14, 30),
-      endDate: new Date(2018, 6, 27, 16, 0),
-    }, {
-      title: 'Submit New Website Design',
-      startDate: new Date(2018, 6, 27, 16, 30),
-      endDate: new Date(2018, 6, 27, 18, 0),
-    }, {
-      title: 'Launch New Website',
-      startDate: new Date(2018, 6, 26, 12, 20),
-      endDate: new Date(2018, 6, 26, 14, 0),
-    }, {
-      title: 'Website Re-Design Plan',
-      startDate: new Date(2018, 6, 16, 9, 30),
-      endDate: new Date(2018, 6, 16, 15, 30),
-    }, {
-      title: 'Book Flights to San Fran for Sales Trip',
-      startDate: new Date(2018, 6, 16, 12, 0),
-      endDate: new Date(2018, 6, 16, 13, 0),
-    }, {
-      title: 'Install New Database',
-      startDate: new Date(2018, 6, 17, 15, 45),
-      endDate: new Date(2018, 6, 18, 12, 15),
-    }, {
-      title: 'Approve New Online Marketing Strategy',
-      startDate: new Date(2018, 6, 18, 12, 35),
-      endDate: new Date(2018, 6, 18, 14, 15),
-    }, {
-      title: 'Upgrade Personal Computers',
-      startDate: new Date(2018, 6, 19, 15, 15),
-      endDate: new Date(2018, 6, 20, 20, 30),
-    }, {
-      title: 'Prepare 2015 Marketing Plan',
-      startDate: new Date(2018, 6, 20, 20, 0),
-      endDate: new Date(2018, 6, 20, 13, 30),
-    }, {
-      title: 'Brochure Design Review',
-      startDate: new Date(2018, 6, 20, 14, 10),
-      endDate: new Date(2018, 6, 20, 15, 30),
-    }, {
-      title: 'Vacation',
-      startDate: new Date(2018, 5, 22),
-      endDate: new Date(2018, 6, 1),
-    }, {
-      title: 'Vacation',
-      startDate: new Date(2018, 6, 28),
-      endDate: new Date(2018, 7, 7),
-    },
+	{
+	  id: 0,
+	  title: 'Watercolor Landscape',
+	  startDate: new Date(2018, 6, 23, 9, 30),
+	  endDate: new Date(2018, 6, 23, 11, 30),
+	  ownerId: 1,
+	}, {
+	  id: 1,
+	  title: 'Monthly Planning',
+	  startDate: new Date(2018, 5, 28, 9, 30),
+	  endDate: new Date(2018, 5, 28, 11, 30),
+	  ownerId: 1,
+	}, {
+	  id: 2,
+	  title: 'Recruiting students',
+	  startDate: new Date(2018, 6, 9, 12, 0),
+	  endDate: new Date(2018, 6, 9, 13, 0),
+	  ownerId: 2,
+	}, {
+	  id: 3,
+	  title: 'Oil Painting',
+	  startDate: new Date(2018, 6, 18, 14, 30),
+	  endDate: new Date(2018, 6, 18, 15, 30),
+	  ownerId: 2,
+	}, {
+	  id: 4,
+	  title: 'Open Day',
+	  startDate: new Date(2018, 6, 20, 12, 0),
+	  endDate: new Date(2018, 6, 20, 13, 35),
+	  ownerId: 6,
+	}, {
+	  id: 5,
+	  title: 'Watercolor Landscape',
+	  startDate: new Date(2018, 6, 6, 13, 0),
+	  endDate: new Date(2018, 6, 6, 14, 0),
+	  rRule: 'FREQ=WEEKLY;BYDAY=FR;UNTIL=20180816',
+	  exDate: '20180713T100000Z,20180727T100000Z',
+	  ownerId: 2,
+	}, {
+	  id: 6,
+	  title: 'Meeting of Instructors',
+	  startDate: new Date(2018, 5, 28, 12, 0),
+	  endDate: new Date(2018, 5, 28, 12, 30),
+	  rRule: 'FREQ=WEEKLY;BYDAY=TH;UNTIL=20180727',
+	  exDate: '20180705T090000Z,20180719T090000Z',
+	  ownerId: 5,
+	}, {
+	  id: 7,
+	  title: 'Oil Painting for Beginners',
+	  startDate: new Date(2018, 6, 3, 11, 0),
+	  endDate: new Date(2018, 6, 3, 12, 0),
+	  rRule: 'FREQ=WEEKLY;BYDAY=TU;UNTIL=20180801',
+	  exDate: '20180710T080000Z,20180724T080000Z',
+	  ownerId: 3,
+	}, {
+	  id: 8,
+	  title: 'Watercolor Workshop',
+	  startDate: new Date(2018, 6, 9, 11, 0),
+	  endDate: new Date(2018, 6, 9, 12, 0),
+	  ownerId: 3,
+	},
   ];
 
 const TeacherScheduler = () => {
   const [data, setData] = useState(appointments);
-  const [currentViewName, setCurrentViewName] = useState('work-week');
+  const [currentViewName, setCurrentViewName] = useState('');
   const [addedAppointment, setAddedAppointment] = useState({});
   const [appointmentChanges, setAppointmentChanges] = useState({});
   const [editingAppointment, setEditingAppointment] = useState(undefined);
 
   const commitChanges = ({ added, changed, deleted }) => {
     setData((prevData) => {
-      let newData = [...prevData];
-      if (added) {
-        const startingAddedId = newData.length > 0 ? newData[newData.length - 1].id + 1 : 0;
-        newData = [...newData, { id: startingAddedId, ...added }];
-      }
-      if (changed) {
-        newData = newData.map(appointment => (
-          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
-        ));
-      }
-      if (deleted !== undefined) {
-        newData = newData.filter(appointment => appointment.id !== deleted);
-      }
-      // Update backend here (added/changed/deleted)
-      return newData;
+		let newData = [...prevData];
+		if (added) 
+		{
+			console.log(added);
+			const startingAddedId = newData.length > 0 ? newData[newData.length - 1].id + 1 : 0;
+			newData = [...newData, { id: startingAddedId, ...added }];
+		}
+		if (changed) 
+		{
+			console.log(changed);
+			newData = newData.map(appointment => (
+			changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
+		));
+		}
+		if (deleted !== undefined) 
+		{
+			console.log(deleted);
+			newData = newData.filter(appointment => appointment.id !== deleted);
+		}
+		// Update backend here (added/changed/deleted)
+		console.log(newData);
+		return newData;
     });
   };
 
@@ -174,8 +141,8 @@ const TeacherScheduler = () => {
               editingAppointment={editingAppointment}
               onEditingAppointmentChange={setEditingAppointment}
             />
-            <WeekView startDayHour={10} endDayHour={19} />
-            <WeekView name="work-week" displayName="Work Week" excludedDays={[0, 6]} startDayHour={9} endDayHour={19} />
+            <WeekView startDayHour={8} endDayHour={19} />
+            {/* <WeekView name="work-week" displayName="Work Week" excludedDays={[0, 6]} startDayHour={9} endDayHour={19} /> */}
             <MonthView />
             <DayView />
             <Toolbar />
